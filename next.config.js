f4051322ecaa608d00ca0c12b6c03c1db2378e4e
Alexander Kühhas <alexander.kuehhas@tttech-auto.com>
2023-11-02 15:17:07 +0100

    NOISUE-1 edit ceo, add pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa');
+
 const nextConfig = {
     // images: {
     //     domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh']
     // }
+    ...withPWA({
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+    })
 };
 
 module.exports = nextConfig;
